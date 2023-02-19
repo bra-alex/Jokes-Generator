bd2a1ee73829b32cbfac21ff87e03dabf63abc56
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-02-19 14:53:16 +0000

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Don Bouncy on 19/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
